e78cbd30318764ea01e490041684a6874c04dc4c
Your Name <you@example.com>
2021-02-09 18:33:28 +0530

    initialize app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <v-app>
     <section style="height: 100vh; width: 100%" class="d-flex align-center justify-center">
       <!-- Login Form -->
+      <v-card max-width="21.875rem">
+        <h1 class="font-weight-bold">Login to Vue Chat App</h1>
+        <v-form>
+          <v-text-field
+            v-model="firstname"
+            :rules="[v => !!v || 'Name is required']"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card>
     </section>
   </v-app>
 </template>
